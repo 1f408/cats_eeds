14dd3b1dd69d8f078d56950a5e2a93264f7a4e1d
k2u2 <97526273+k2u2@users.noreply.github.com>
2024-07-17 13:03:15 +0900

    Updated src.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/1f408/cats_eeds
 go 1.20
 
 require (
-	github.com/alecthomas/chroma/v2 v2.12.0
+	github.com/alecthomas/chroma/v2 v2.14.0
 	github.com/l4go/osfs v1.20240116.0
 	github.com/l4go/recode v1.20240116.0
 	github.com/l4go/rpath v1.20240116.0
@@ -16,14 +16,14 @@ require (
 	github.com/yuin/goldmark v1.6.0
 	github.com/yuin/goldmark-emoji v1.0.2
 	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc
-	golang.org/x/net v0.20.0
-	golang.org/x/text v0.14.0
+	golang.org/x/net v0.27.0
+	golang.org/x/text v0.16.0
 )
 
 require (
-	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
--- view/internal/links/links.go
+++ view/internal/links/links.go
@@ -44,7 +44,7 @@ func new_links_by_file(dirs []string) []Link {
 		case 1:
 			links[i].Path = "."
 		default:
-			links[i].Path = strings.Repeat("../", rev_i-2) + ".."
+			links[i].Path = encode_file_url(strings.Repeat("../", rev_i-2) + "..")
 		}
 	}
 
@@ -61,9 +61,66 @@ func new_links_by_dir(dirs []string) []Link {
 		case 0:
 			links[i].Path = ""
 		default:
-			links[i].Path = strings.Repeat("../", rev_i-1) + ".."
+			links[i].Path = encode_file_url(strings.Repeat("../", rev_i-1) + "..")
 		}
 	}
 
 	return links
+}
+
+func is_encode_file_url(c byte) bool {
+	if 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9' ||
+		c == '-' || c == '.' || c == '_' || c == '~' || c == '/' {
+		return false
+	}
+
+	return true
+}
+
+func EncodeFileURL(p string) string {
+	p = rpath.Clean(p)
+
+	if p == "" {
+		return p
+	}
+	if p[0] != '/' {
+		return "./" + encode_file_url(p)
+	}
+
+	return encode_file_url(p)
+}
+
+func encode_file_url(src string) string {
+	const hex_char = "0123456789ABCDEF"
+
+	esc_cnt := 0
+
+	for i := 0; i < len(src); i++ {
+		c := src[i]
+		if is_encode_file_url(c) {
+			esc_cnt++
+		}
+	}
+	if esc_cnt == 0 {
+		return src
+	}
+
+	req_len := len(src) + 2*esc_cnt
+	dst := make([]byte, req_len)
+
+	j := 0
+	for i := 0; i < len(src); i++ {
+		c := src[i]
+		if is_encode_file_url(c) {
+			dst[j] = '%'
+			dst[j+1] = hex_char[c>>4]
+			dst[j+2] = hex_char[c&0xF]
+			j += 3
+		} else {
+			dst[j] = c
+			j++
+		}
+	}
+
+	return string(dst)
 }
